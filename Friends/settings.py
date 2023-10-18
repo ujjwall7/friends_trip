02050aa46a8078b734f24fec467ca36af9d9c8f6
ujjwal <sharmaujjwal0921@gmail.com>
2023-10-18 16:50:51 +0000

    database

--- Friends/settings.py
+++ Friends/settings.py
@@ -89,23 +89,23 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'Travel_Expense',
-#          'USER': 'Travel_Expense',
-#          'PASSWORD': 'Travel_Expense@Friends',
-#          'HOST': 'localhost',
-#          'PORT': '5432',
-#      }
-# }
+ DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'Travel_Expense',
+          'USER': 'Travel_Expense',
+          'PASSWORD': 'Travel_Expense@Friends',
+          'HOST': 'localhost',
+          'PORT': '5432',
+      }
+ }
 
 
 # Password validation
